3ac42151b45093bb2d048b5af01a2964b8068b66
Nick Giuliani <nicholas.giuliani94@gmail.com>
2021-12-24 15:22:37 -0500

    Fleshed out server.js a bit more

--- server/server.js
+++ server/server.js
@@ -1,2 +1,30 @@
 const express = require('express');
 const {ApolloServer} = require('apollo-server-express');
+const {authMiddleware} = require('./utils/auth');
+const path = require('path');
+const {typeDefs, resolvers} = require('./schemas');
+const db = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({ 
+  typeDefs, 
+  resolvers, 
+  context: authMiddleware 
+});
+
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+    });
+});
