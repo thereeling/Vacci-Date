9a2c82e87fc144f6c1222f07b7e9fc0ef78080f4
Corinne Marshall <corinnemarshall@Corinnes-MacBook-Air.local>
2021-12-27 13:55:18 -0500

    added a NotFound page

---
+++ client/src/pages/NotFound.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+       <div>
+            <h1>404 Page Not Found</h1>
+        <h1>
+          <span role="img" aria-label="Hearts Emoji">
+          💘 + 💘
+          </span>
+        </h1>
+       </div> 
+    );
+};
+
+export default NotFound;
