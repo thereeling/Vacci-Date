041e91e507c50b1e5c6af874b09be729aac3ddc6
Corinne Marshall <corinnemarshall@Corinnes-MacBook-Air.local>
2021-12-27 13:18:06 -0500

    added imports from apollo/client, createhttplink, adding aith link

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,28 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  ApolloClient,
+  inMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+
 
 function App() {
   return (
