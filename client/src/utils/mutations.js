e263dd0dd4cfa961b843bf5a535cf7e00b887580
JPDBrodeur <jbrodeur001@gmail.com>
2021-12-31 07:52:18 -0500

    proper indentation in mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,13 +1,12 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN = gql`
-    mutation Login($username: String!, $password: String!){
+    mutation Login($username: String!, $password: String!) {
         login(username: $username, password: $password) {
-        user {
-            _id
-            
-        }
-        token
+            user {
+                _id   
+            }
+            token
         }
     }
 `;
@@ -15,10 +14,33 @@ export const LOGIN = gql`
 export const ADD_USER = gql`
     mutation AddUser($input: UserInput) {
         addUser(input: $input) {
-        user {
+            user {
+                _id
+                username
+                firstname
+                email
+                age
+                location
+                gender
+                preference
+                agerangemin
+                agerangemax
+                hobbies
+                aboutme
+                likes
+                likedby
+                matches
+            }
+        }
+    }
+`;
+
+export const UPDATE_USER = gql `
+    mutation UpdateUser($input: UserInput) {
+        updateUser(input: $input) {
             _id
-            username
             firstname
+            username
             email
             age
             location
@@ -28,67 +50,44 @@ export const ADD_USER = gql`
             agerangemax
             hobbies
             aboutme
-            likes
-            likedby
-            matches
-        }
         }
     }
-`;
-
-export const UPDATE_USER = gql `
-mutation UpdateUser($input: UserInput){
-    updateUser(input: $input) {
-        _id
-        firstname
-        username
-        email
-        age
-        location
-        gender
-        preference
-        agerangemin
-        agerangemax
-        hobbies
-        aboutme
-    }
-  }
 `;
 
 export const DELETE_USER = gql `
-mutation DeleteUser{
-    deleteUser {
-        _id
-        username
-        firstname
-        email
-        likedby
-        likes
-        matches   
+    mutation DeleteUser {
+        deleteUser {
+            _id
+            username
+            firstname
+            email
+            likedby
+            likes
+            matches   
+        }
     }
-  }
 `;
 
 export const LIKE_USER = gql`
-    mutation Like($_id: ID){
+    mutation Like($_id: ID) {
         like(_id: $_id) {
-        _id
-        likedby
-        likes
-        matches
-        username
-    }
+            _id
+            likedby
+            likes
+            matches
+            username
+        }
     }
 `;
 
 export const UNLIKE_USER = gql`
-    mutation UnLike($_id: ID){
+    mutation UnLike($_id: ID) {
         unlike(_id: $_id) {
-        _id
-        likes
-        likedby
-        matches
-        username
+            _id
+            likes
+            likedby
+            matches
+            username
         }
     }
 `;
